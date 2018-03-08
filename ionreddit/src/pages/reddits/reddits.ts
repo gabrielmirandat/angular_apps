9624a3b6b91ad246267b45cec203577c80962e09
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-08 15:32:30 -0300

    Finalizing >Ionic 2 Mobile App in Under 60 Minutes<

--- ionreddit/src/pages/reddits/reddits.ts
+++ ionreddit/src/pages/reddits/reddits.ts
@@ -9,12 +9,29 @@ import { DetailsPage } from '../details/details'
 })
 export class RedditsPage {
   itens: any;
+  category: any;
+  limit: any;
   constructor(public navCtrl: NavController, 
               private redditService:RedditService) {
+    this.getDefaults();
   }
 
   ngOnInit () {
-    this.getPosts('sports', 10)
+    this.getPosts(this.category, this.limit)
+  }
+
+  getDefaults() {
+    if(localStorage.getItem('category') != null){
+      this.category = localStorage.getItem('category');
+    }else{
+      this.category = 'sports';
+    }
+
+    if(localStorage.getItem('limit') != null){
+      this.limit = localStorage.getItem('limit');
+    }else{
+      this.limit = 10;
+    }
   }
 
   getPosts (category, limit) {
@@ -27,6 +44,10 @@ export class RedditsPage {
     this.navCtrl.push(DetailsPage, {
       item:item
     })
+  }
+
+  changeCategory() {
+    this.getPosts(this.category, this.limit)
   }
 
 }
