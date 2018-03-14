431cfb284ba95cff8bc55a8abeb7b41d5f35647f
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-14 18:25:51 -0300

    Adding front crud with users

---
+++ fullstack/a4app_web/src/app/components/user/details-user/details-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-user',
+  templateUrl: './details-user.component.html'
+})
+export class DetailsUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
