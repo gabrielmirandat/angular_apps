1ff3ec687529b38b14cead17b5e5a5e2f5e8bea6
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-15 15:26:46 -0300

    Moving fullstack project to another repo

--- fullstack/a4app_server/routes/car.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const { car } = app.controllers;
-
-  app.get('/cars', car.getAll);
-  app.get('/cars/:id', car.get);
-  app.post('/cars', car.add);
-  app.put('/cars/:id', car.update);
-  app.delete('/cars/:id', car.delete);
-};
--- fullstack/a4app_web/src/app/components/user/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface User{
-  id:number;
-  name:string;
-  email:string;
-  age:number;
-  street:string;
-  city:string;
-  state:string;
-}
